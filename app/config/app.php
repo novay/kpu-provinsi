7f75e5697461c14cd523fff0d742b86b12f5cbb0
Novay Mawbowo <novay@otaku.si>
2014-01-11 15:33:17 +0800

    Change secret key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'ogqWbU5pNRdTCvxs1EUdyqqHdoNkcKVi',
 
 	/*
 	|--------------------------------------------------------------------------
