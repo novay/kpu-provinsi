4aa6f8316526a59f855ed3f1b5fd0a4ee5121392
Novay Mawbowo <novay@otaku.si>
2014-01-11 15:03:52 +0800

    Little change

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,12 @@
 <?php
 
-Route::get('/', 'App\\Controllers\\HomeController@showWelcome');
+/**
+ *
+ * ====================================
+ * Aplikasi Persentase Kekuatan Wilayah
+ * ====================================
+ * @author Novay Mawbowo ('novay@otaku.si')
+ * 
+ */
+
+Route::get('/', 'App\\Controllers\\HomeController@getIndex');
