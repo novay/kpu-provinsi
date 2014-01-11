601c9f37f5ded3ef76b96cffed10eab30ef280cb
Novay Mawbowo <novay@otaku.si>
2014-01-11 15:33:36 +0800

    Database driver change

--- app/config/database.php
+++ app/config/database.php
@@ -55,12 +55,12 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'kpu-provinsi',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'admin',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'prefix'    => 'vay_',
 		),
 
 		'pgsql' => array(
